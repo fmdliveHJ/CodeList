a4c69f148ccdfc56303a99796afdcd4d7a597d2f
hyeonjin <gdalove@naver.com>
2023-01-23 22:12:18 +0900

    form 요소

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,27 +1,67 @@
 import React, { useState } from "react";
 import "./ReviewForm.css";
 const ReviewForm = () => {
-  const [title, setTitle] = useState("");
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState("");
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
+  // const [title, setTitle] = useState("");
+  // const [rating, setRating] = useState(0);
+  // const [content, setContent] = useState("");
+
+  /**
+   * state 하나로 죽이기
+   */
+  //  const handleTitleChange = (e) => {
+  //   setTitle(e.target.value);
+  // };
+
+  // const handleRatingChange = (e) => {
+  //   const nextRating = Number(e.target.value) || 0;
+  //   setRating(nextRating);
+  // };
+
+  // const handleContentChange = (e) => {
+  //   setContent(e.target.value);
+  // };
+
+  const [values, setValues] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
 
-  const handleRatingChange = (e) => {
-    const nextRating = Number(e.target.value) || 0;
-    setRating(nextRating);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      values,
+    });
   };
 
   return (
-    <form className="ReviewForm">
-      <input type="text" value={title} onChange={handleTitleChange} />
-      <input type="number" value={rating} onChange={handleRatingChange} />
-      <textarea value={content} onChange={handleContentChange}></textarea>
+    <form className="ReviewForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="title"
+        value={values.title}
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        name="rating"
+        value={values.rating}
+        onChange={handleChange}
+      />
+      <textarea
+        name="content"
+        value={values.content}
+        onChange={handleChange}
+      ></textarea>
+      <button type="submit">확인 </button>
     </form>
   );
 };
